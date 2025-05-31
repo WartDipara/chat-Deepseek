17035105bb7515579c596c395c8517ea6d7be8fe
Wart <zxc290322281@yahoo.com>
2025-05-31 10:39:47 +0800

    add advisors

--- src/main/java/com/wart/ai/config/CommonConfiguration.java
+++ src/main/java/com/wart/ai/config/CommonConfiguration.java
@@ -1,6 +1,7 @@
 package com.wart.ai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,9 @@ import org.springframework.context.annotation.Configuration;
 public class CommonConfiguration {
     @Bean
     public ChatClient chatClient(OllamaChatModel model) {
-        return ChatClient.builder(model)
+        return ChatClient
+                .builder(model)
+                .defaultAdvisors(new SimpleLoggerAdvisor()) // 添加一个简单的日志记录器，配置需要的日志级别，默认是debug级别，去yaml文件中配置
                 .build();
     }
 }
